d465d1f10c24461b60197a2dfecd17e260714749
Nikita_Hnitetskyi <108988090+SmyslovaVictoria@users.noreply.github.com>
2023-06-13 14:03:01 +0300

    update

--- js/task-01.js
+++ js/task-01.js
@@ -5,14 +5,14 @@ const categoriesQuantity = categories.children.length;
 console.log("Number of categories:", categoriesQuantity);
 
 [...items].forEach((i) => {
-    
+
   const itemTitle = i.querySelector("h2");
 
   const titleCategory = itemTitle.textContent;
 
   const categoryItems = i.querySelectorAll("li").length;
 
-    console.log("Category", titleCategory);
-    console.log("items", categoryItems);
+    console.log("Category:", titleCategory);
+    console.log("Elements:", categoryItems);
     
 });
