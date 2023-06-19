fb21586ac477870ac06eb93578531e91c52c4f38
Nikita_Hnitetskyi <108988090+SmyslovaVictoria@users.noreply.github.com>
2023-06-19 15:34:09 +0300

    update

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const form = document.querySelector(".change-color").addEventListener("click", (e) => {
+  console.log("Button was clicked");
+  
+  const body = document.querySelector("body");
+  const span = document.querySelector(".color");
+
+    if (e.currentTarget) {
+      body.style.backgroundColor = getRandomHexColor();
+      span.innerHTML = getRandomHexColor();
+    }
+  });
+
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,39 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  let boxSize = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = boxSize + "px";
+    box.style.height = boxSize + "px";
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxesContainer.appendChild(box);
+    boxSize += 10;
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+
+createBtn.addEventListener("click", function () {
+  const input = document.querySelector("#controls input");
+  const amount = parseInt(input.value, 10);
+
+  if (!isNaN(amount)) {
+    createBoxes(amount);
+  } else {
+    return alert("Please enter a valid number!");
+  }
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
